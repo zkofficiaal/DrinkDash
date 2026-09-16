90be6b78baf1b67804be418e4ec97ccf6972b4b4
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-09-16 09:11:48 -0700

    Add ProductSize enum with small, medium, and large cases

--- DrinkDash/Models/ProductSize.swift
+++ DrinkDash/Models/ProductSize.swift
@@ -5,3 +5,19 @@
 //  Created by Z.K   on 16/09/2026.
 //
 
+import Foundation
+
+enum ProductSize: String, CaseIterable, Identifiable, Codable {
+
+    case small = "S"
+    case medium = "M"
+    case large = "L"
+
+    var id: String {
+        rawValue
+    }
+
+    var displayName: String {
+        rawValue
+    }
+}
