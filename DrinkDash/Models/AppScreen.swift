52f5d0a7d43f40be9775795f18a72c6e0009bd3e
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-09-16 09:11:08 -0700

    Define AppScreen enum with home, product detail, and receipt cases

---
+++ DrinkDash/Models/AppScreen.swift
@@ -0,0 +1,13 @@
+//
+//  AppScreen.swift
+//  DrinkDash
+//
+//  Created by Z.K   on 16/09/2026.
+//
+
+enum AppScreen: Equatable {
+
+    case home
+    case productDetail(Product)
+    case receipt(Order)
+}
