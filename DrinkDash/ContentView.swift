72dae6dc57b90cd2ba7c9f1cfb027069d014eb8e
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-09-15 18:35:13 -0700

    Initial Commit

---
+++ DrinkDash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DrinkDash
+//
+//  Created by Z.K   on 15/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DrinkDash/DrinkDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkDashApp.swift
+//  DrinkDash
+//
+//  Created by Z.K   on 15/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct DrinkDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
