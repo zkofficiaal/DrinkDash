dc46efe832df9cab1413a12ccc5b90dbbb6555e8
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-09-16 09:21:20 -0700

    Add AppSpacing enum with standardized layout constants for margins and hero sections

--- DrinkDash/DesignSystem/AppSpacing.swift
+++ DrinkDash/DesignSystem/AppSpacing.swift
@@ -5,3 +5,19 @@
 //  Created by Z.K   on 16/09/2026.
 //
 
+import Foundation
+
+enum AppSpacing {
+
+    static let horizontal: CGFloat = 24
+
+    static let small: CGFloat = 8
+    static let medium: CGFloat = 14
+    static let large: CGFloat = 22
+    static let extraLarge: CGFloat = 32
+
+    static let heroTop: CGFloat = 90
+    static let heroBottom: CGFloat = 120
+
+    static let bottomButton: CGFloat = 24
+}
