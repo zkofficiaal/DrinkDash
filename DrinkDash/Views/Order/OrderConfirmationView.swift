a34cc9fb3be733189bad3cd879badf3f7557bb39
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-09-16 09:51:50 -0700

    Add OrderConfirmationView with staged receipt animation and tap-to-dismiss navigation

--- DrinkDash/Views/Order/OrderConfirmationView.swift
+++ DrinkDash/Views/Order/OrderConfirmationView.swift
@@ -5,3 +5,51 @@
 //  Created by Z.K   on 16/09/2026.
 //
 
+import SwiftUI
+
+// MARK: - Order Confirmation View
+
+struct OrderConfirmationView: View {
+
+    @Environment(AppRouter.self) private var router
+
+    @State private var viewModel: OrderViewModel
+
+    init(order: Order) {
+        _viewModel = State(
+            initialValue: OrderViewModel(
+                order: order
+            )
+        )
+    }
+
+    var body: some View {
+
+        ZStack {
+
+            AppColors.darkBackground
+                .ignoresSafeArea()
+
+            if viewModel.isReceiptVisible {
+
+                ReceiptView(
+                    order: viewModel.order,
+                    viewModel: viewModel
+                )
+            }
+        }
+        .task {
+            viewModel.startReceiptAnimation()
+        }
+        .onTapGesture {
+
+            guard viewModel.isThankYouVisible else {
+                return
+            }
+
+            withAnimation(AppMotion.screenTransition) {
+                router.showHome()
+            }
+        }
+    }
+}
