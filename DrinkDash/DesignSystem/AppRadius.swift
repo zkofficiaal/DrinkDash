efdd88d6f4e99e8367934e888e56229573640e91
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-09-16 10:11:38 -0700

    Add AppRadius constants for consistent corner radii and include asset images for products

---
+++ DrinkDash/DesignSystem/AppRadius.swift
@@ -0,0 +1,18 @@
+//
+//  AppRadius.swift
+//  DrinkDash
+//
+//  Created by Z.K   on 16/09/2026.
+//
+
+import SwiftUI
+
+struct AppRadius {
+
+    static let small: CGFloat = 10
+    static let medium: CGFloat = 16
+    static let large: CGFloat = 24
+    static let extraLarge: CGFloat = 32
+
+    private init() {}
+}
